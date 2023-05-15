14a00dc39d0a11352a2584231475fd0bd82bbf74
adriano barbosa <adriano.barbosa100@gmail.com>
2023-05-15 15:59:17 -0300

    feat: operadores relacionais

---
+++ 03-operadores-relacional/aula03.py
@@ -0,0 +1,9 @@
+# operadores relacionais
+
+compare = 2 == 2 #retorno True bool
+diferenca = 2 != 2 #retorno False bool
+maior = 2 > 2 # retorno False bool
+menor = 2 < 2 # retorno False bool
+maior_ou_igual = 2 >= 2 #retorno True bool
+menor_ou_igual = 2 <= 2 #retorno True bool
+
