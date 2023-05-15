92022bb7d80ff6f0f8a24de0493a21f83bd16a4f
adriano barbosa <adriano.barbosa100@gmail.com>
2023-05-15 16:00:22 -0300

    feat: tuplas

---
+++ 09-tuplas/aula.py
@@ -0,0 +1,14 @@
+# diferenças de listas e tuplas
+# as tuplas são imutaveis, não da para alterar elementos de uma tupla
+
+notas = (4, 5)
+
+pessoa = (
+    "Adriano",
+    35,
+    True
+)
+
+#para percorrer a tupla usamos o desempacotamento, onde setamos as variaveis
+# de acordo com o index da tupla
+nome, idade, admin = pessoa 
