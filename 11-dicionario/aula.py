45aa44139587b6ee565f543d97e36406b02a8962
adriano barbosa <adriano.barbosa100@gmail.com>
2023-05-15 16:00:43 -0300

    feat: dicionario

---
+++ 11-dicionario/aula.py
@@ -0,0 +1,15 @@
+#dicionarios, estruturas muito ultilizadas
+
+#parecidos com objetos javascript
+pessoa = {
+    "nome": "Jhon dow",
+    "idade": 45,
+    "endereco": {
+        "rua": "rua das acacias",
+        "numero": "18"
+    }
+}
+
+print(pessoa["nome"])
+print(pessoa["endereco"])
+print(pessoa["endereco"]["numero"])
