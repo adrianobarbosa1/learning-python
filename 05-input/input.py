d26dfb877165989c7f338ca21bb4c9a2e283d1bf
adriano barbosa <adriano.barbosa100@gmail.com>
2023-05-22 14:35:02 -0300

    feat: add input

--- 05-input/input.py
+++ 05-input/input.py
@@ -1,3 +1,10 @@
-idade = input("Digite a idade:\n") #valor recebido sempre em string
-idade = int(idade) #convertendo string para inteiro
+# input é o comando para o usuario entrar com
+# algum dado no programa 
+
+idade = input("Digite a idade:\n") 
+#valor recebido sempre em string
+
+idade = int(idade)
+ #convertendo string para inteiro
+ 
 print(idade)
