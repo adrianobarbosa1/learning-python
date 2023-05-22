2cc47e812b78e59fbacf4a28db01441b0433e5cc
adriano barbosa <adriano.barbosa100@gmail.com>
2023-05-22 14:36:03 -0300

    feat: add functions

---
+++ 13-função/aula.py
@@ -0,0 +1,17 @@
+# funcao
+
+def minha_funcao():
+    print("ola" )
+
+minha_funcao() #vai printar o olá
+
+#parametros
+def pessoa(nome, sobrenome, idade):
+    print(nome)
+    print(sobrenome)
+    print(idade)
+
+pessoa("adriano", "barbosa", 20)
+
+#argumentos nomeados
+pessoa(nome="adriano", sobrenome="barbosa", idade=17)
